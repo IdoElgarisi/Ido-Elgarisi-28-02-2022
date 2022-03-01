2f34cd8a9b9102748ef6aa2060be9330c74c63e2
IdoElgarisi <ido12el@gmail.com>
2022-03-01 14:42:47 +0200

    adding dist subtree

---
+++ dist/precache-manifest.cf46c8eb351eeb31c7643e602c22cb91.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56335f2437f66f5b0a0",
+    "url": "/Ido-Elgarisi-28-02-2022/css/app.b312a765.css"
+  },
+  {
+    "revision": "44d80505d3750fd08bcd12fb967faec5",
+    "url": "/Ido-Elgarisi-28-02-2022/img/1.44d80505.svg"
+  },
+  {
+    "revision": "9e47e57c684bca9740cd8c5b7ec64c61",
+    "url": "/Ido-Elgarisi-28-02-2022/img/12.9e47e57c.svg"
+  },
+  {
+    "revision": "6a0c111e6ef66300605a1b99f8e84029",
+    "url": "/Ido-Elgarisi-28-02-2022/img/13.6a0c111e.svg"
+  },
+  {
+    "revision": "7f3ac0b6e0f4115d47d26e325a836c04",
+    "url": "/Ido-Elgarisi-28-02-2022/img/14.7f3ac0b6.svg"
+  },
+  {
+    "revision": "7f3ac0b6e0f4115d47d26e325a836c04",
+    "url": "/Ido-Elgarisi-28-02-2022/img/18.7f3ac0b6.svg"
+  },
+  {
+    "revision": "c6da91199d684f65531ce8ebeac3afbc",
+    "url": "/Ido-Elgarisi-28-02-2022/img/2.c6da9119.svg"
+  },
+  {
+    "revision": "0d6558a64e48a5d51422d3356e57aab2",
+    "url": "/Ido-Elgarisi-28-02-2022/img/22.0d6558a6.svg"
+  },
+  {
+    "revision": "e6773627ead16603dc6b353167652073",
+    "url": "/Ido-Elgarisi-28-02-2022/img/3.e6773627.svg"
+  },
+  {
+    "revision": "44d80505d3750fd08bcd12fb967faec5",
+    "url": "/Ido-Elgarisi-28-02-2022/img/33.44d80505.svg"
+  },
+  {
+    "revision": "6626d0ff7ceef56de6dea4d6d45cf43e",
+    "url": "/Ido-Elgarisi-28-02-2022/img/34.6626d0ff.svg"
+  },
+  {
+    "revision": "fad7ed3e72d9037f22b0c043a55cc26c",
+    "url": "/Ido-Elgarisi-28-02-2022/img/4.fad7ed3e.svg"
+  },
+  {
+    "revision": "91de39328b1a0cdb29c1025d6f007f64",
+    "url": "/Ido-Elgarisi-28-02-2022/img/6.91de3932.svg"
+  },
+  {
+    "revision": "8a8e7eafd58874beb6cad04751117055",
+    "url": "/Ido-Elgarisi-28-02-2022/img/7.8a8e7eaf.svg"
+  },
+  {
+    "revision": "e4f97f8ebb5553ec9024ea5610699b1a",
+    "url": "/Ido-Elgarisi-28-02-2022/index.html"
+  },
+  {
+    "revision": "8cb2349917d995247691",
+    "url": "/Ido-Elgarisi-28-02-2022/js/about.b5063dfc.js"
+  },
+  {
+    "revision": "b56335f2437f66f5b0a0",
+    "url": "/Ido-Elgarisi-28-02-2022/js/app.5cafb381.js"
+  },
+  {
+    "revision": "ad34db538f20dd3cc5ef",
+    "url": "/Ido-Elgarisi-28-02-2022/js/chunk-vendors.b18f7a26.js"
+  },
+  {
+    "revision": "04f03e59d9481f48cf5770bbfe7e1f94",
+    "url": "/Ido-Elgarisi-28-02-2022/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Ido-Elgarisi-28-02-2022/robots.txt"
+  }
+]);
